18aac444369f98c47ab9e0c1f952d8977177dde0
ParachutePenguin <sst7092000@yahoo.co.jp>
2023-02-26 13:19:31 +0000

    test

---
+++ test.jl
@@ -0,0 +1,14 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+#> [frontmatter]
+#> title = "test"
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 674e3f78-f9bd-484c-b58a-cc07e66a7913
+1+1
+
+# ╔═╡ Cell order:
+# ╠═674e3f78-f9bd-484c-b58a-cc07e66a7913
