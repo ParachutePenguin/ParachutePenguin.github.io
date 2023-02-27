c316c06c94e6419556dea301d9627aa9e7057da4
ParachutePenguin <sst7092000@yahoo.co.jp>
2023-02-27 12:48:52 +0000

    test

--- test.jl
+++ test.jl
@@ -10,5 +10,9 @@ using InteractiveUtils
 # ╔═╡ 674e3f78-f9bd-484c-b58a-cc07e66a7913
 1+3
 
+# ╔═╡ 413d4a2c-d795-48aa-b1bd-11f1da73e538
+pi
+
 # ╔═╡ Cell order:
 # ╠═674e3f78-f9bd-484c-b58a-cc07e66a7913
+# ╠═413d4a2c-d795-48aa-b1bd-11f1da73e538
